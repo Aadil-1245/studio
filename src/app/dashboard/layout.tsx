c6b2796aa4c197381f1e45389f0b437f3d080c1e
aadil aadhi <avpgamingstation@gmail.com>
2025-08-15 11:39:57 +0000

    we should have an option the slide it slide out like that

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { DashboardNav } from '@/components/dashboard-nav';
-import { TooltipProvider } from '@/components/ui/tooltip';
+import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
 
 export default function DashboardLayout({
   children,
@@ -7,17 +7,15 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <TooltipProvider>
-      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-        <div className="hidden border-r bg-muted/40 md:block">
-          <DashboardNav />
-        </div>
-        <div className="flex flex-col">
-          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-            {children}
-          </main>
-        </div>
-      </div>
-    </TooltipProvider>
+    <SidebarProvider>
+      <Sidebar>
+        <DashboardNav />
+      </Sidebar>
+      <SidebarInset>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
